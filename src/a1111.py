0c21b1aa9b678fb7d127afcca892782f51dbf8d3
Jason Livesay <runvnc@gmail.com>
2024-05-28 07:27:39 +0000

    update

--- src/a1111.py
+++ src/a1111.py
@@ -1,8 +1,7 @@
-from auto1111sdk import StableDiffusionXLPipeline
+from auto1111sdk import StableDiffusionXLPipeline, civit_download
 import sys
 import uuid
-
-pipe = StableDiffusionXLPipeline("models/model.safetensors")
+import os
 
 def gen(prompt, neg='', width=768, height=1024, steps=10, cfg_scale=7.5):
     output = pipe.generate_txt2img(prompt = prompt, negative_prompt=neg, cfg_scale=cfg_scale, height = height, width = width, steps = steps)
@@ -12,10 +11,18 @@ def gen(prompt, neg='', width=768, height=1024, steps=10, cfg_scale=7.5):
     output[0].save(fname)
     return fname
 
-prompt = sys.argv[1]
-neg = sys.argv[2]
+modelurl = sys.argv[1]
+modelname = sys.argv[2]
+prompt = sys.argv[3]
+neg = sys.argv[4]
+
+if not os.path.exists(f"models/{modelname}.safetensors"):
+    print(f"Model not found, downloading {modelurl} to {modelname}")
+    civit_download(model, f"models/{modelname}.safetensors")
+
+pipe = StableDiffusionXLPipeline(f"models/{modelname}.safetensors")
 
 for i in range(1, 5):
-    fname = gen(prompt, neg=neg, cfg_scale=11, steps=27)
+    fname = gen(prompt, neg=neg, cfg_scale=8, steps=25)
     print(fname)
 
