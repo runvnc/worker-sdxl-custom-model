55914331b179b93482fd6cf674fbc041c882d7bd
Jason Livesay <runvnc@gmail.com>
2024-05-28 07:04:16 +0000

    update

--- src/a1111.py
+++ src/a1111.py
@@ -4,8 +4,8 @@ import uuid
 
 pipe = StableDiffusionXLPipeline("models/model.safetensors")
 
-def gen(prompt, neg, width, height, steps):
-    output = pipe.generate_txt2img(prompt = prompt, negative_prompt=neg, height = 1024, width = 768, steps = 10)
+def gen(prompt, neg='', width=768, height=1024, steps=10, cfg_scale=7.5):
+    output = pipe.generate_txt2img(prompt = prompt, negative_prompt=neg, cfg_scale=cfg_scale, height = height, width = width, steps = steps)
 
     fname = f"{uuid.uuid4()}.png"
 
@@ -15,7 +15,7 @@ def gen(prompt, neg, width, height, steps):
 prompt = sys.argv[1]
 neg = sys.argv[2]
 
-for i in range 1..4:
-    fname = gen(prompt, neg)
+for i in range(1, 5):
+    fname = gen(prompt, neg=neg, cfg_scale=11, steps=27)
     print(fname)
 
