c51ae479cda61fd8f34bb5c79adcbfcc09701811
Jason Livesay <runvnc@gmail.com>
2024-06-02 08:08:03 +0000

    update

--- src/a1111.py
+++ src/a1111.py
@@ -11,18 +11,20 @@ def gen(prompt, neg='', width=768, height=1024, steps=10, cfg_scale=7.5):
     output[0].save(fname)
     return fname
 
-modelurl = sys.argv[1]
-modelname = sys.argv[2]
-prompt = sys.argv[3]
-neg = sys.argv[4]
+if __name__ == '__main__':
+    modelurl = sys.argv[1]
+    modelname = sys.argv[2]
+    prompt = sys.argv[3]
+    neg = sys.argv[4]
 
-if not os.path.exists(f"models/{modelname}.safetensors"):
-    print(f"Model not found, downloading {modelurl} to {modelname}")
-    civit_download(modelurl, f"models/{modelname}.safetensors")
+    if not os.path.exists(f"models/{modelname}.safetensors"):
+        print(f"Model not found, downloading {modelurl} to {modelname}")
+        civit_download(modelurl, f"models/{modelname}.safetensors")
 
-pipe = StableDiffusionXLPipeline(f"models/{modelname}.safetensors")
+    pipe = StableDiffusionXLPipeline(f"models/{modelname}.safetensors")
+    pipe.set_vae("models/sdxl_vae.safetensors")
 
-for i in range(1, 5):
-    fname = gen(prompt, neg=neg, cfg_scale=9, steps=25)
-    print(fname)
+    for i in range(1, 5):
+        fname = gen(prompt, neg=neg, cfg_scale=9, steps=25)
+        print(fname)
 
