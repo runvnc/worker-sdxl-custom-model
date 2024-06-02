a2f11ca287d13ab8582176caf47a85a95effb5a9
Jason Livesay <runvnc@gmail.com>
2024-06-02 08:17:11 +0000

    update

--- src/a1111.py
+++ src/a1111.py
@@ -3,6 +3,8 @@ import sys
 import uuid
 import os
 
+pipe = None
+
 def gen(prompt, neg='', width=768, height=1024, steps=10, cfg_scale=7.5):
     output = pipe.generate_txt2img(prompt = prompt, negative_prompt=neg, cfg_scale=cfg_scale, height = height, width = width, steps = steps)
 
@@ -11,7 +13,7 @@ def gen(prompt, neg='', width=768, height=1024, steps=10, cfg_scale=7.5):
     output[0].save(fname)
     return fname
 
-if __name__ == '__main__':
+if False and __name__ == '__main__':
     modelurl = sys.argv[1]
     modelname = sys.argv[2]
     prompt = sys.argv[3]
@@ -27,4 +29,7 @@ if __name__ == '__main__':
     for i in range(1, 5):
         fname = gen(prompt, neg=neg, cfg_scale=9, steps=25)
         print(fname)
+else:
+    pipe = StableDiffusionXLPipeline(f"models/model.safetensors")
+    pipe.set_vae("models/sdxl_vae.safetensors")
 
--- src/rp_handler.py
+++ src/rp_handler.py
@@ -2,26 +2,8 @@
 Contains the handler function that will be called by the serverless.
 '''
 
+import a1111
 import os
-import io
-import base64
-import concurrent.futures
-import torch
-import torchvision
-from torchvision import transforms
-
-from diffusers import StableDiffusionXLPipeline, StableDiffusionXLImg2ImgPipeline, AutoencoderKL
-from diffusers.utils import load_image
-from diffusers import DiffusionPipeline
-
-from diffusers import (
-    PNDMScheduler,
-    LMSDiscreteScheduler,
-    DDIMScheduler,
-    EulerDiscreteScheduler,
-    DPMSolverMultistepScheduler,
-)
-
 import runpod
 from runpod.serverless.utils import rp_upload, rp_cleanup
 from runpod.serverless.utils.rp_validator import validate
@@ -29,41 +11,6 @@ from runpod.serverless.utils.rp_validator import validate
 from rp_schemas import INPUT_SCHEMA
 
 torch.cuda.empty_cache()
-
-# ------------------------------- Model Handler ------------------------------ #
-
-
-class ModelHandler:
-    def __init__(self):
-        self.base = None
-        self.refiner = None
-        self.load_models()
-
-    def load_base(self):
-        print("loading base")
-        vae = AutoencoderKL.from_pretrained(
-            "madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16)
- 
-        base_pipe = StableDiffusionXLPipeline.from_single_file(
-            "models/model.safetensors"
-            , torch_dtype       = torch.float16
-            , use_safetensors   = True
-            , variant           = "fp16"
-            , vae               = vae
-            , custom_pipeline   = "lpw_stable_diffusion_xl",
-        )
-        base_pipe = base_pipe.to("cuda", silence_dtype_warnings=True)
-        base_pipe.enable_xformers_memory_efficient_attention()
-        return base_pipe
-
-    def load_models(self):
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future_base = executor.submit(self.load_base)
-
-            self.base = future_base.result()
-
-
-MODELS = ModelHandler()
 
 # ---------------------------------- Helper ---------------------------------- #
 
@@ -75,63 +22,15 @@ def _save_and_upload_images(images, job_id):
         image_path = os.path.join(f"/{job_id}", f"{index}.png")
         image.save(image_path)
 
-        if os.environ.get('BUCKET_ENDPOINT_URL', False):
-            image_url = rp_upload.upload_image(job_id, image_path)
-            image_urls.append(image_url)
-        else:
-            with open(image_path, "rb") as image_file:
-                image_data = base64.b64encode(
-                    image_file.read()).decode("utf-8")
-                image_urls.append(f"data:image/png;base64,{image_data}")
+       with open(image_path, "rb") as image_file:
+            image_data = base64.b64encode(
+                image_file.read()).decode("utf-8")
+            image_urls.append(f"data:image/png;base64,{image_data}")
 
     rp_cleanup.clean([f"/{job_id}"])
 
     return image_urls
 
-
-def make_scheduler(name, config):
-    return {
-        "PNDM": PNDMScheduler.from_config(config),
-        "KLMS": LMSDiscreteScheduler.from_config(config),
-        "DDIM": DDIMScheduler.from_config(config),
-        "K_EULER": EulerDiscreteScheduler.from_config(config),
-        "DPMSolverMultistep": DPMSolverMultistepScheduler.from_config(config),
-    }[name]
-
-def get_pipeline_embeds(pipeline, prompt, negative_prompt, device):
-    """ Get pipeline embeds for prompts bigger than the maxlength of the pipe
-    :param pipeline:
-    :param prompt:
-    :param negative_prompt:
-    :param device:
-    :return:
-    """
-    max_length = pipeline.tokenizer.model_max_length
-
-    # simple way to determine length of tokens
-    count_prompt = len(prompt.split(" "))
-    count_negative_prompt = len(negative_prompt.split(" "))
-
-    # create the tensor based on which prompt is longer
-    if count_prompt >= count_negative_prompt:
-        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False).input_ids.to(device)
-        shape_max_length = input_ids.shape[-1]
-        negative_ids = pipeline.tokenizer(negative_prompt, truncation=False, padding="max_length",
-                                          max_length=shape_max_length, return_tensors="pt").input_ids.to(device)
-
-    else:
-        negative_ids = pipeline.tokenizer(negative_prompt, return_tensors="pt", truncation=False).input_ids.to(device)
-        shape_max_length = negative_ids.shape[-1]
-        input_ids = pipeline.tokenizer(prompt, return_tensors="pt", truncation=False, padding="max_length",
-                                       max_length=shape_max_length).input_ids.to(device)
-
-    concat_embeds = []
-    neg_embeds = []
-    for i in range(0, shape_max_length, max_length):
-        concat_embeds.append(pipeline.text_encoder(input_ids[:, i: i + max_length])[0])
-        neg_embeds.append(pipeline.text_encoder(negative_ids[:, i: i + max_length])[0])
-
-    return torch.cat(concat_embeds, dim=1), torch.cat(neg_embeds, dim=1)
 
 
 @torch.inference_mode()
@@ -148,49 +47,32 @@ def generate_image(job):
         return {"error": validated_input['errors']}
     job_input = validated_input['validated_input']
 
-    starting_image = job_input['image_url']
-
     if job_input['seed'] is None:
         job_input['seed'] = int.from_bytes(os.urandom(2), "big")
 
-    generator = torch.Generator("cuda").manual_seed(job_input['seed'])
-
-    MODELS.base.scheduler = make_scheduler(job_input['scheduler'], MODELS.base.scheduler.config)
     pipeline = MODELS.base
-    compel = Compel(tokenizer=[pipeline.tokenizer, pipeline.tokenizer_2] , text_encoder=[pipeline.text_encoder, pipeline.text_encoder_2], returned_embeddings_type=ReturnedEmbeddingsType.PENULTIMATE_HIDDEN_STATES_NON_NORMALIZED, truncate_long_prompts=False, requires_pooled=[False, True])
-    conditioning = compel.build_conditioning_tensor(job_input['prompt']))
-    negative_conditioning = compel.build_conditioning_tensor(job_input['negative_prompt'])
-    [conditioning, negative_conditioning] = compel.pad_conditioning_tensors_to_same_length([conditioning, negative_conditioning])
+    fname = gen(prompt, neg=neg, cfg_scale=9, steps=25)
     
-    image = pipeline(prompt_embeds=conditioning, pooled_prompt_embeds=pooled,
-                     negative_prompt_embeds=negative_conditioning, 
-                     pooled_negative_prompt_embeds=num_inference_steps=30).images[0]
-
-    output = MODELS.base.txt2img(
-        prompt_embeds=conditioning,
-        pooled_prompt_embeds=pooled,
+    fname = a1111.gen(
+        prompt=job_input['prompt'],
         negative_prompt=job_input['negative_prompt'],
         height=job_input['height'],
         width=job_input['width'],
-        max_embeddings_multiples=3,
-        num_inference_steps=job_input['num_inference_steps'],
-        guidance_scale=job_input['guidance_scale'],
-        denoising_end=job_input['high_noise_frac'],
-        #output_type="latent",
-        num_images_per_prompt=job_input['num_images'],
-        generator=generator
+        steps=job_input['num_inference_steps'],
+        cfg_scale=job_input['guidance_scale']
         ).images
 
-    image_urls = _save_and_upload_images(output, job['id'])
+    image_urls = []
+    with open(image_path, "rb") as image_file:
+        image_data = base64.b64encode(
+            image_file.read()).decode("utf-8")
+        image_urls.append(f"data:image/png;base64,{image_data}")
 
     results = {
         "images": image_urls,
         "image_url": image_urls[0],
         "seed": job_input['seed']
     }
-
-    if starting_image:
-        results['refresh_worker'] = True
 
     return results
 
